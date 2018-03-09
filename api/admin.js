b7f3075f32ffaec87620519cd49ed053d9b7e05f
Sami <megasamsi@gmail.com>
2018-03-09 23:02:51 +0100

    Implemented 5 methods for admin-part

--- api/admin.js
+++ api/admin.js
@@ -1,41 +1,125 @@
 const express = require('express');
 const router = express.Router();
-const mongo = require('mongodb');
-const mongoUrl = 'mongodb://localhost:27017/bookingdb'
+const mongoClient = require('mongodb').MongoClient;
+const mongodb = require('mongodb');
 
-router.get('/', function(request, response, next){
-  console.log(request.params);
-  response.status(200).json({
-    message: 'GET requests to /products'
+const mongoUrl = 'mongodb://localhost:27017';
+
+router.get('/getReservations', function(request, response, next){
+  mongoClient.connect(mongoUrl, function(error, database){
+    const outPut = database.db('bookingdb').collection('reservations').find();
+
+    outPut.toArray(function(err, data) {
+      response.json({'reservations' : data});
+    });
+
+    database.close();
   });
 });
 
-router.post('/', function(request, response, next){
-  console.log(request.body.navn);
-  response.status(201).json({
-    message: 'POST requests to /products'
+router.get('/getRooms', function(request, response, next){
+  mongoClient.connect(mongoUrl, function(error, database){
+    const outPut = database.db('bookingdb').collection('rooms').find();
+
+    outPut.toArray(function(err, data) {
+      response.json({'rooms' : data});
+    });
+
+    database.close();
   });
 });
 
-router.get('/:productId', function(request, response, next){
-  const id = request.params.productId;
-  response.status(200).json({
-    message: 'You discovered the special ID',
-    id: id
+router.post('/insertReservation', function(request, response, next){
+  const inputData = {
+    name: request.body.name,
+    duration: request.body.duration
+  };
+
+  mongoClient.connect(mongoUrl, function(error, database){
+    database.db('bookingdb').collection('reservations').insertOne(inputData, function(error, result){
+      if(error){
+        response.json(error);
+      }
+      else{
+        response.status(200).json({message: 'Record inserted'});
+      }
+      database.close();
+    });
   });
 });
 
-router.patch('/:productId', function(request, response, next){
-  response.status(200).json({
-    message: 'Updated product!'
+router.post('/insertRoom', function(request, response, next){
+  const inputData = {
+    room: request.body.room,
+    size: request.body.size
+  };
+
+  mongoClient.connect(mongoUrl, function(error, database){
+    database.db('bookingdb').collection('reservations').insertOne(inputData, function(error, result){
+      if(error){
+        response.json(error);
+      }
+      else{
+        response.status(200).json({message: 'Record inserted'});
+      }
+      database.close();
+    });
   });
 });
 
-router.delete('/:productId', function(request, response, next){
-  response.status(200).json({
-    message: 'Deleted product!'
+router.delete('/deleteReservation/:reservationId', function(request, response, next){
+  const resId = {_id: new mongodb.ObjectID(request.params.reservationId)};
+
+  mongoClient.connect(mongoUrl, function(error, database){
+    database.db('bookingdb').collection('reservations').deleteOne(resId, function(error, result){
+      if(error){
+        response.json(error);
+      }
+      else{
+        response.status(200).json({message: 'Record deleted'});
+      }
+      database.close();
+    });
+  });
+
+});
+
+router.delete('/deleteRoom/:roomId', function(request, response, next){
+  const roomId = {_id: new mongodb.ObjectID(request.params.roomId)};
+
+  mongoClient.connect(mongoUrl, function(error, database){
+    database.db('bookingdb').collection('reservations').deleteOne(roomId, function(error, result){
+      if(error){
+        response.json(error);
+      }
+      else{
+        response.status(200).json({message: 'Record deleted'});
+      }
+      database.close();
+    });
+  });
+
+});
+
+router.patch('/updateReservation/:reservationId', function(request, response, next){
+  const resId = {_id: new mongodb.ObjectID(request.params.roomId)};
+  const updateObj = {$set: request.body};
+
+  mongoClient.connect(mongoUrl, function(error, database){
+    database.db('bookingdb').collection('reservations').updateOne(resId, updateObj, function(error, result){
+      if (error){
+        response.json(error);
+      }
+      else{
+        response.status(200).json({message: 'Record update'});
+      }
+      database.close();
+    });
+
   });
 });
+
+
 
 
 module.exports = router;
--- api/client.js
+++ api/client.js
@@ -1,43 +1,42 @@
 const express = require('express');
 const router = express.Router();
-const mongo = require('mongodb').MongoClient;
+const mongoClient = require('mongodb').MongoClient;
 const mongoUrl = 'mongodb://localhost:27017';
 
 
-router.get('/getData', function(request, response, next){
+router.get('/getRooms', function(request, response, next){
 
-  var resultArray = [];
+  mongoClient.connect(mongoUrl, function(error, database){
+    const outPut = database.db('bookingdb').collection('rooms').find();
 
-  mongo.connect(mongoUrl, function(error, database){
-    var outPut = database.db('bookingdb').collection('rooms').find();
-    outPut.toArray(function(err, students) {
-              // so now, we can return all students to the screen.
-              response.status(200).json({'rooms' : students});
-           });
+    outPut.toArray(function(err, data) {
+      response.json({'rooms' : data});
+    });
 
-           database.close();
-
+    database.close();
   });
 
 });
 
-router.post('/insertData', function(request, response, next){
+router.post('/insertReservation', function(request, response, next){
 
   const inputData = {
     name: request.body.name,
     room: request.body.room
   };
 
-  mongo.connect(mongoUrl, function(error, database){
+  mongoClient.connect(mongoUrl, function(error, database){
     database.db('bookingdb').collection('reservations').insertOne(inputData, function(error, result){
       if (error){
        console.log('hahahaha');
-     }else{
-      console.log('hohohohho');
-    }
+     }
+     else{
+       console.log('hohohohho');
+     }
     database.close();
     });
   });
+
 });
 
 module.exports = router;
