c50388416aa64a35b7a57c437e0585189d2a9294
samsebamse <megasamsi@gmail.com>
2018-03-11 14:37:26 +0100

    Fixed minor issues and implemented one more http method

--- api/admin.js
+++ api/admin.js
@@ -1,10 +1,13 @@
+// Importing libraries
 const express = require('express');
 const router = express.Router();
 const mongoClient = require('mongodb').MongoClient;
 const mongodb = require('mongodb');
 
+// Url to database
 const mongoUrl = 'mongodb://localhost:27017';
 
+// Read all records in reservations collection
 router.get('/getReservations', function(request, response, next){
   mongoClient.connect(mongoUrl, function(error, database){
     const outPut = database.db('bookingdb').collection('reservations').find();
@@ -17,6 +20,7 @@ router.get('/getReservations', function(request, response, next){
   });
 });
 
+// Read all records in rooms collection
 router.get('/getRooms', function(request, response, next){
   mongoClient.connect(mongoUrl, function(error, database){
     const outPut = database.db('bookingdb').collection('rooms').find();
@@ -29,6 +33,7 @@ router.get('/getRooms', function(request, response, next){
   });
 });
 
+// Create a new record in reservations collection
 router.post('/insertReservation', function(request, response, next){
   const inputData = {
     name: request.body.name,
@@ -48,6 +53,7 @@ router.post('/insertReservation', function(request, response, next){
   });
 });
 
+// Create a new record in rooms collection
 router.post('/insertRoom', function(request, response, next){
   const inputData = {
     room: request.body.room,
@@ -55,7 +61,7 @@ router.post('/insertRoom', function(request, response, next){
   };
 
   mongoClient.connect(mongoUrl, function(error, database){
-    database.db('bookingdb').collection('reservations').insertOne(inputData, function(error, result){
+    database.db('bookingdb').collection('rooms').insertOne(inputData, function(error, result){
       if(error){
         response.json(error);
       }
@@ -67,6 +73,25 @@ router.post('/insertRoom', function(request, response, next){
   });
 });
 
+// Slett reservasjon uten å sende URL input
+/*router.delete('/deleteReservation', function(request, response, next){
+  const resId = {_id: new mongodb.ObjectID(request.body.id)};
+
+  mongoClient.connect(mongoUrl, function(error, database){
+    database.db('bookingdb').collection('reservations').deleteOne(resId, function(error, result){
+      if(error){
+        response.json(error);
+      }
+      else{
+        response.status(200).json({message: 'Record deleted'});
+      }
+      database.close();
+    });
+  });
+
+});*/
+
+// Delete a record in reservations collection
 router.delete('/deleteReservation/:reservationId', function(request, response, next){
   const resId = {_id: new mongodb.ObjectID(request.params.reservationId)};
 
@@ -84,11 +109,12 @@ router.delete('/deleteReservation/:reservationId', function(request, response, n
 
 });
 
+// Delete a record in rooms collection
 router.delete('/deleteRoom/:roomId', function(request, response, next){
   const roomId = {_id: new mongodb.ObjectID(request.params.roomId)};
 
   mongoClient.connect(mongoUrl, function(error, database){
-    database.db('bookingdb').collection('reservations').deleteOne(roomId, function(error, result){
+    database.db('bookingdb').collection('rooms').deleteOne(roomId, function(error, result){
       if(error){
         response.json(error);
       }
@@ -101,8 +127,9 @@ router.delete('/deleteRoom/:roomId', function(request, response, next){
 
 });
 
+// Update a record in reservations collection
 router.patch('/updateReservation/:reservationId', function(request, response, next){
-  const resId = {_id: new mongodb.ObjectID(request.params.roomId)};
+  const resId = {_id: new mongodb.ObjectID(request.params.reservationId)};
   const updateObj = {$set: request.body};
 
   mongoClient.connect(mongoUrl, function(error, database){
@@ -119,7 +146,24 @@ router.patch('/updateReservation/:reservationId', function(request, response, ne
   });
 });
 
+// Update a record in rooms collection
+router.patch('/updateRooms/:roomId', function(request, response, next){
+  const roomId = {_id: new mongodb.ObjectID(request.params.roomId)};
+  const updateObj = {$set: request.body};
 
+  mongoClient.connect(mongoUrl, function(error, database){
+    database.db('bookingdb').collection('rooms').updateOne(resId, updateObj, function(error, result){
+      if (error){
+        response.json(error);
+      }
+      else{
+        response.status(200).json({message: 'Record update'});
+      }
+      database.close();
+    });
+
+  });
+});
 
 
 module.exports = router;
